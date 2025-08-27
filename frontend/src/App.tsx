5355f29d3951f8f6651db6d217070014617d80d7
Manjunaths21n <manjunanths21@gmail.com>
2025-08-27 19:43:05 +0530

    chore: resolved code review comments (#3)
    * chore: update with required changes
    
    * chore: added router support
    
    * chore: resolved code review comment
    
    ---------
    
    Co-authored-by: Manjunath S <manjunaths21@gmail.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,4 @@
-import { Container } from '@mui/material';
-import { ButtonAppBar, EnhancedTable } from './components';
-import { createRoutesFromElements, Route, createBrowserRouter, RouterProvider } from 'react-router';
+import { RouterProvider } from 'react-router';
 import { storeManagerBrowserRouter } from './routes/routes';
 
 function App() {
@@ -13,22 +11,3 @@ function App() {
 }
 
 export default App
-
-
-// Usually used in a declarative router
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       <Routes>
-//         <Route index element={<StepOne />} />
-//         <Route path="step-2" element={<StepTwo />} />
-//         <Route path="step-3" element={<StepThree />} />
-//       </Routes>
-//    </BrowserRouter>
-//   );
-// }
-
-
-// function App2() {
-//   return <RouterProvider router={storeManagerBrowserRouter} />;
-// }
