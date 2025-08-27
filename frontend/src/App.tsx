c0418fe36f67aca02469d6ecdd0f516b91844938
Manjunath S <manjunaths21@gmail.com>
2025-08-27 19:37:11 +0530

    chore: resolved code review comment

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,11 @@
-import { Container } from '@mui/material';
-import { ButtonAppBar, EnhancedTable } from './components';
-import { createRoutesFromElements, Route, createBrowserRouter, RouterProvider } from 'react-router';
+import { RouterProvider } from 'react-router';
 import { storeManagerBrowserRouter } from './routes/routes';
 
 function App() {
 
   return (
-    <>
-      <RouterProvider router={storeManagerBrowserRouter} />
-    </>
+    <RouterProvider router={storeManagerBrowserRouter} />
   );
 }
 
 export default App
-
-
-// Usually used in a declarative router
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       <Routes>
-//         <Route index element={<StepOne />} />
-//         <Route path="step-2" element={<StepTwo />} />
-//         <Route path="step-3" element={<StepThree />} />
-//       </Routes>
-//    </BrowserRouter>
-//   );
-// }
-
-
-// function App2() {
-//   return <RouterProvider router={storeManagerBrowserRouter} />;
-// }
